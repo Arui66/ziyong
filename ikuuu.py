7f1322fe2f553db863716f9f8925cc7014562435
Arui66 <62711026+Arui66@users.noreply.github.com>
2023-01-14 22:50:32 +0800

    Update ikuuu.py

--- ikuuu.py
+++ ikuuu.py
@@ -3,13 +3,13 @@ import requests, json,re
 #https://ikuuu.dev/auth/register?code=C1yx 这个是注册链接 一个免费的比较稳定的机场可以 
 #https://ikuuu.dev/user登录后cookie
 
-cookie ='lang=zh-cn; uid=825633; email=757442495@qq.com; key=5db734ec5e7153d2d461705124aa635df52be53d50d7d; ip=db373a5d20d83e860cf1ebdbf3d61d02; expire_in=1673369458; _ga=GA1.2.483709748.1672764661; _gid=GA1.2.1639946823.1672764661'
+cookie ='lang=zh-cn; _ga=GA1.2.483709748.1672764661; uid=825633; email=757442495@qq.com; key=d23a1fa51e2d14659477c3025199111c6e98c8bb19c8e; ip=ac9e41b0763aed7ebc5a19e4f0955d3e; expire_in=1674312510'
 
 url_info = 'https://ikuuu.dev/user/profile'
 url = 'https://ikuuu.dev/user/checkin'
 headers = {
     'cookie': f'{cookie}',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
 }
 html_info = requests.get(url=url_info, headers=headers).text
 html = requests.post(url=url, headers=headers)
