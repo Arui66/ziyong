6890f239a1e7b10397484903306f444b3154f3a0
Arui66 <62711026+Arui66@users.noreply.github.com>
2023-02-09 14:00:31 +0800

    Update ikuuu.py

--- yangmao/ikuuu.py
+++ yangmao/ikuuu.py
@@ -3,7 +3,7 @@ import requests, json,re
 #https://ikuuu.dev/auth/register?code=C1yx 这个是注册链接 一个免费的比较稳定的机场可以 
 #https://ikuuu.dev/user登录后cookie
 
-cookie ='lang=zh-cn; _ga=GA1.2.483709748.1672764661; uid=825633; email=757442495@qq.com; key=d23a1fa51e2d14659477c3025199111c6e98c8bb19c8e; ip=ac9e41b0763aed7ebc5a19e4f0955d3e; expire_in=1674312510'
+cookie ='lang=zh-cn; _ga=GA1.2.483709748.1672764661; uid=825633; email=757442495@qq.com; key=edeef5e50ddd8acdff7a051e0dc10dadbf62c14ba92df; ip=d0f5cb98e82d0f8c46154581032b707f; expire_in=1676527109; _gid=GA1.2.343446607.1675922312; _gat_gtag_UA_158605448_1=1'
 
 url_info = 'https://ikuuu.dev/user/profile'
 url = 'https://ikuuu.dev/user/checkin'
