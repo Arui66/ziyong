31d09ba07e893c903da9e5dbb2289d4be70851ad
Arui66 <62711026+Arui66@users.noreply.github.com>
2023-04-17 01:15:58 +0800

    小米社区任务得成长值

--- yangmao/miui.py
+++ yangmao/miui.py
@@ -1,28 +1,33 @@
-import requests, json ,time,base64,binascii,hashlib,os
+#!/usr/bin/python3
+# -- coding: utf-8 --
+# -------------------------------
+# @Author : github@wd210010 https://github.com/wd210010/just_for_happy
+# @Time : 2023/2/27 13:23
+# -------------------------------
+# cron "30 8,10,15 * * *" script-path=xxx.py,tag=匹配cron用
+# const $ = new Env('小米社区任务得成长值')
+# 4.11 更新了请求后获取cookie失败的问题 基本一次性就可以跑完 不用多跑几次
+
+import requests, json ,time,base64,binascii,hashlib,os,re
 
 # 小米签到 小米社区任务得成长值
 # 配置帐号密码 一一对应 按需增删 不对上会出错 若帐号密码填写没有错误 还是报错应该是账号在非常用设备上登录, 需要验证码, 使用该设备安装图形化工具后自行前去验证https://web-alpha.vip.miui.com/page/info/mio/mio/internalTest 图形化工具怎么安装可参考https://cloud.tencent.com/developer/article/2069955
-# account =['帐号1','帐号2','账号3']
-# password =['密码1','密码2','密码3']
-"""
-cron: 57 0,9 * * *
-new Env("小米社区任务得成长值");
-"""
+# 青龙变量export mi_account='' export mi_password=''
+
 # 青龙变量 mi_account mi_password
 mi_account = os.getenv("mi_account").split('&')
 mi_password = os.getenv("mi_password").split('&')
 
-
-# 获取cookie
-def Phone(mi_account, mi_password):
+#获取cookie
+def Phone(account, password):
     md5 = hashlib.md5()
-    md5.update(mi_password.encode())
+    md5.update(password.encode())
     Hash = md5.hexdigest()
     url = "https://account.xiaomi.com/pass/serviceLoginAuth2"
     headers = {
         "Content-Type": "application/x-www-form-urlencoded",
         "User-Agent":
-        "Dalvik/2.1.0 (Linux; U; Android 12; 21051182C Build/SKQ1.220303.001) APP/xiaomi.vipaccount APPV/230113 MK/WGlhb21pIFBhZCA1 SDKV/5.1.0.release PassportSDK/5.1.0.release passport-ui/5.1.0.release",
+        "Dalvik/2.1.0 (Linux; U; Android 12; M2007J17C Build/SKQ1.211006.001) APP/xiaomi.vipaccount APPV/220301 MK/UmVkbWkgTm90ZSA5IFBybw== PassportSDK/3.7.8 passport-ui/3.7.8",
         "Cookie":
         "deviceId=X0jMu7b0w-jcne-S; pass_o=2d25bb648d023d7f; sdkVersion=accountsdk-2020.01.09",
         "Host": "account.xiaomi.com",
@@ -34,15 +39,15 @@ def Phone(mi_account, mi_password):
         "qs": "%3F_json%3Dtrue%26sid%3Dmiui_vip%26_locale%3Dzh_CN",
         "callback": "https://api.vip.miui.com/sts",
         "_json": "true",
-        "user": mi_account,
+        "user": account,
         "hash": Hash.upper(),
         "sid": "miui_vip",
         "_sign": "ZJxpm3Q5cu0qDOMkKdWYRPeCwps%3D",
         "_locale": "zh_CN"
     }
-    Auth = requests.post(url=url, headers=headers,
+    Auth1 = requests.post(url=url, headers=headers,
                          data=data).text.replace("&&&START&&&", "")
-    Auth = json.loads(Auth)
+    Auth = json.loads(Auth1)
     ssecurity = Auth["ssecurity"]
     nonce = Auth["nonce"]
     sha1 = hashlib.sha1()
@@ -54,37 +59,37 @@ def Phone(mi_account, mi_password):
     nurl = Auth[
         "location"] + "&_userIdNeedEncrypt=true&clientSign=" + clientSign
 
-    sts = requests.get(url=nurl)
-    return requests.utils.dict_from_cookiejar(sts.cookies)
+    resp = requests.get(url=nurl)
+    return requests.utils.dict_from_cookiejar(resp.cookies)
 
-#签到任务
+
+
 for i in range(len(mi_account)):
-    cookie = str(Phone(f'{mi_account[i]}', f'{mi_password[i]}')).replace('{','').replace('}','').replace(',',';').replace(': ','=').replace('\'','').replace(' ','')
-    url = 'https://api.vip.miui.com/mtop/planet/vip/user/checkin'
+    c_list = []
+    for k in range(10):
+        a = Phone(mi_account[i], mi_password[i])
+        if len(a) > 0:
+            c_list.append(a)
+    cookie = str(c_list[-1]).replace('{','').replace('}','').replace(',',';').replace(': ','=').replace('\'','').replace(' ','')
+    miui_vip_ph = "".join(re.findall('miui_vip_ph=(.*?);', cookie, re.S))
+    url = 'https://api.vip.miui.com/mtop/planet/vip/user/checkin?pathname=/mio/checkIn&version=dev.1144'
     headers = {
-        'Host': 'api.vip.miui.com',
-        'Connection': 'keep-alive',
-        'User-Agent': 'Mozilla/5.0 (Linux; U; Android 12; zh-cn; M2007J1SC Build/RKQ1.200826.002) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/89.0.4389.116 Mobile Safari/537.36 XiaoMi/MiuiBrowser/15.7.22 app/vipaccount',
-        'Accept': '*/*',
-        'Origin': 'https://web.vip.miui.com',
-        'X-Requested-With': 'com.xiaomi.vipaccount',
-        'Sec-Fetch-Site': 'same-site',
-        'Sec-Fetch-Mode': 'cors',
-        'Sec-Fetch-Dest': 'empty',
-        'Referer': 'https://web.vip.miui.com/page/info/mio/mio/checkIn?app_version=dev.220804',
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
         'Cookie': f'{cookie}'
     }
     user_url = 'https://api.vip.miui.com/api/community/user/home/page'
-    html = requests.get(url=url, headers=headers)
+    params = {
+            'miui_vip_ph': miui_vip_ph
+    }
+    html = requests.get(url=url, headers=headers,params=params)
     html_user = requests.get(url=user_url, headers=headers)
     result = json.loads(html.text)
     result_user = json.loads(html_user.text)
     userId = result_user['entity']['userId']
     print('*************'+'\n'+f'开始第{i + 1}个账号签到'+'\n'+'签到结果：')
+    print(result['message'])
     print('userId: '+userId + ' 用户名: '+result_user['entity']['userName']+ ' 段位: '+ result_user['entity']['userGrowLevelInfo']['showLevel'])
-    print(result)
+
 # 点赞任务
     print('开始加入点赞任务>>>>')
     for a in range(2):
@@ -148,5 +153,3 @@ for i in range(len(mi_account)):
             print('浏览帖子成功，获得积分： ' + str(result_watch['entity']['score']))
         else:
             print(result_watch['message'] + '，今日已达上限')
-
-    time.sleep(60)
